dee3593d41f32fdf03d38ab6626a01ac17474a40
PRIYA BHATT <41810819+ipriyabhatt@users.noreply.github.com>
2022-06-04 16:16:41 +0530

    Add files via upload

---
+++ a.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int sum,x,y;
+    print("Enter no:");
+    scanf("%d" & x);
+    print("enter no:");
+    scanf("%d" & y);
+    sum=x+y;
+    print("the sum is %d",sum);
+    return 0;
+}
